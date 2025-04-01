5a9c5a55fc23a316b276ae8dcdc4195c70e39aa4
Alisher-00kg <alisher.estebesov@gmail.com>
2025-04-01 12:31:34 +0300

    fixed styles

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,118 +1,81 @@
 import React from "react";
-import styled, { css } from "styled-components";
-
-const Button = ({ children, type, onClick, variant, disabled, ...props }) => {
+import styled from "styled-components";
+export const Button = ({
+  children,
+  onClick,
+  disabled,
+  variant,
+  type,
+  ...props
+}) => {
   return (
-    <>
-      <div>
-        <StyledButton
-          type={type}
-          onClick={onClick}
-          variant={variant}
-          disabled={disabled}
-          {...props}
-        >
-          {children}
-        </StyledButton>
-      </div>
-    </>
+    <StyledButton
+      onClick={onClick}
+      disabled={disabled}
+      variant={variant}
+      type={type}
+      {...props}
+    >
+      {children}
+    </StyledButton>
   );
 };
-
-export default Button;
-
-const StyledButton = styled.button`
-  width: ${(props) =>
-    props.variant === "sign"
-      ? 440
-      : props.variant === "add"
-      ? 99
-      : props.variant === "close"
-      ? 110
-      : props.variant === "cart"
-      ? 249
-      : "none"};
-  height: ${(props) =>
-    props.variant === "sign"
-      ? 50
-      : props.variant === "add"
-      ? 41
-      : props === "close"
-      ? 44
-      : props.variant === "cart"
-      ? 59
-      : "none"};
-  border: ${(props) => (props.variant === "close" ? "#8A2B06" : "none")};
-  cursor: pointer;
-  border-radius: ${(props) =>
-    props.variant === "sign"
-      ? 15
-      : props.variant === "add"
-      ? 20
-      : props.variant === "cart"
-      ? 30
-      : "none"};
-  color: ${(props) =>
-    props.variant === "sign"
-      ? "#FAFAFA"
-      : props.variant === "add"
-      ? "#FFFFFF"
-      : props.variant === "close"
-      ? "#8A2B06"
-      : props.variant === "cart"
-      ? "#FFFFFF"
-      : "none"};
-
-  ${({ variant }) => {
-    switch (variant) {
-      case "sign":
-        return css`
-          background-color: #060874;
-          &:hover {
-            background-color: #151783;
-          }
-          &:active {
-            background-color: #101039;
-          }
-        `;
-      case "add":
-        return css`
-          background-color: #8a2b06;
-          &:hover {
-            background-color: #7e2a0a;
-          }
-          &:active {
-            background-color: #993108;
-          }
-          &:disabled {
-            background-color: #ffffff;
-          }
-        `;
-      case "close":
-        return css`
-          background-color: #fafafa0;
-          &:hover {
-            background-color: #8a2b06;
-          }
-          &:active {
-            background-color: #993108;
-          }
-          &:disabled {
-            background-color: #cac6c4;
-          }
-        `;
-      case "card":
-        return css`
-          background-color: #5a1f08;
-          &:hover {
-            background-color: #782d10;
-          }
-          &:active {
-            background-color: #431b0c;
-          }
-        `;
-      default:
-        return null;
+const StylesButton = (variant) => {
+  switch (variant) {
+    case "add": {
+      return {
+        width: "99px",
+        height: "41px",
+        background: "rgb(138, 43, 6)",
+        color: "white",
+        fontSize: "14px",
+        fontWeight: "700",
+        lineHeight: "21px",
+        letterSpacing: "3%",
+        "&:hover": {
+          background: "rgb(126, 42, 10)",
+        },
+        "&:active": {
+          background: "rgb(153, 49, 8)",
+        },
+        "&:disabled": {
+          background: "rgb(202, 198, 196)",
+        },
+      };
     }
-  }}
+    case "close": {
+      return {
+        width: "110px",
+        height: "44px",
+        borderRadius: "20px",
+        border: "1px solid rgb(138, 43, 6)",
+        background: "transparent",
+        color: "rgb(138, 43, 6)",
+        fontSize: "16px",
+        lineHeight: "24px",
+        "&:hover": {
+          background: "rgb(138, 43, 6)",
+          color: "white",
+        },
+        "&:active": {
+          background: "rgb(153, 49, 8)",
+        },
+        "&:disabled": {
+          border: "1px solid rgb(202, 198, 196)",
+          color: "#CAC6C4",
+          background: "transparent",
+        },
+      };
+    }
+  }
+};
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+  ${(props) => StylesButton(props.variant)}
 `;
