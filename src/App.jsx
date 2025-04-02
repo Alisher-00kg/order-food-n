a02b4b5808ebcd0347e18debc9cf99ec3c65b04d
Kubat <Kubatb990@gmail.com>
2025-04-02 20:43:08 +0300

    save Modal card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { ModalItemCard } from "../src/components/UI/ModalItemCard";
-import { foods } from "./utils/constants/foods";
 const App = () => {
   return <div>App</div>;
 };
