dc4b691bdfaa78972e49d88f48a9caf9bf404dde
Argen <raxmanberdievargen577@gmail.com>
2025-04-03 19:24:41 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import React from "react";
-import HeaderSecond from "./components/HeaderSecond";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
 const App = () => {
-  return (
-    <div>
-      <HeaderSecond />
-      <Main />\
-      <Footer />
-    </div>
-  );
+  return <div>Add</div>;
 };
 
 export default App;
